83be31badff1266e593f2a4085043600844e2513
0x262d <71982379+canarddu38@users.noreply.github.com>
2025-12-15 16:20:21 +0100

    Add files via upload

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,1 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcleme <julcleme@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 17:05:31 by julcleme          #+#    #+#             */
+/*   Updated: 2025/11/11 14:13:13 by julcleme         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	nb;
+	int	sign;
+	int	i;
+
+	i = 0;
+	nb = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		if ((size_t)nb > (size_t)(LLONG_MAX / 10) ||
+			((size_t)nb == (size_t)(LLONG_MAX / 10) &&
+			(size_t)(str[i] - '0') > (size_t)(LLONG_MAX % 10)))
+			return (-(sign == 1));
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nb * sign);
+}
