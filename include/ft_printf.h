27136cdcdd97f05d8bbfd60a2626a71bd5db328c
0x262d <71982379+canarddu38@users.noreply.github.com>
2025-12-15 16:22:41 +0100

    Add files via upload

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -1,1 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcleme <julcleme@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/11 11:13:37 by julcleme          #+#    #+#             */
+/*   Updated: 2025/11/15 00:05:56 by julcleme         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h> 
+
+# define FLAG_MINUS 0
+# define FLAG_ZERO 1
+# define FLAG_HASHTAG 2
+# define FLAG_SPACE 3
+# define FLAG_PLUS 4
+# define WIDTH_UNRESTRICTED -1
+
+typedef struct s_format
+{
+	char	flags[6];
+	int		width;
+	int		precision;
+	char	type;
+}	t_format;
+
+t_format	parse_format(char *str, size_t *i);
+
+int			ft_putnbr_count(int nb, t_format f, int pad_len, int prec_len);
+int			ft_putstr_count(char *str, t_format f);
+int			ft_putchar_count(char c, t_format f);
+int			ft_uint_putnbr_count(uint32_t nb, t_format f, int pad, int prec);
+int			ft_putptr_count(void *ptr, t_format f);
+int			ft_puthex_count(uint32_t i, t_format f, int pad_len, int prec_len);
+
+int			display_hex(size_t nb, int in_lowercase);
+int			display_uint(unsigned int nb);
+
+int			ft_printf(const char *s, ...) __attribute__((format(printf, 1, 2)));
+
+int			ft_put_precision_rep(int already_printed);
+int			ft_put_padding_rep(int count, char pad);
+
+int			max(int i, int j);
+int			min(int i, int j);
+
+#endif
---
+++ include/push_swap_bonus.h
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcleme <julcleme@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 12:32:32 by sservant          #+#    #+#             */
+/*   Updated: 2025/12/08 17:57:41 by julcleme         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
+
+# include "libft.h"
+# include <unistd.h>
+# define STACK_BLOCK_SIZE 4096
+# define ERR_EMPTY		1
+# define MODE_NORMAL 0
+# define MODE_SIMPLE 1
+# define MODE_MEDIUM 2
+# define MODE_COMPLEX 3
+# define MODE_ADAPTIVE 4
+
+# define MAX_VISUAL_SIZE 30
+
+# ifndef IS_VISUAL
+#  define IS_VISUAL 0
+# endif
+
+typedef struct s_stack
+{
+	long	stack_top_idx;
+	int		*arr;
+	int		*rank;
+	int		capacity;
+}	t_stack;
+
+typedef struct s_bench
+{
+	int	ops;
+	int	disorder;
+	int	sa;
+	int	sb;
+	int	ss;
+	int	pa;
+	int	pb;
+	int	ra;
+	int	rb;
+	int	rr;
+	int	rra;
+	int	rrb;
+	int	rrr;
+	int	mode;
+	int	strat;
+}	t_bench;
+
+int		simple_push_swap(t_stack *a, t_bench *bench);
+void	medium_push_swap(t_stack *a, t_bench *bench);
+void	complex_push_swap(t_stack *a, t_bench *bench);
+void	adaptive_push_swap(t_stack *a, t_bench *bench);
+
+void	parse_stack_rank(t_stack *a, int size);
+void	visual_bench(t_stack *a, t_stack *b, int scale);
+void	create_benchmark(t_bench *bench);
+void	init_bench(t_bench *bench);
+int		init_stack(t_stack *stack);
+void	free_stack(t_stack *stack);
+int		push(int i, t_stack *stack);
+int		pop(t_stack *stack, int *output);
+int		is_empty(t_stack *stack);
+int		top(t_stack	*stack, int *output);
+int		get_max_index(t_stack *s);
+int		get_max_value(t_stack *s);
+
+int		swap_a(t_stack *a, t_bench *bench);
+int		swap_b(t_stack *b, t_bench *bench);
+int		swap_s(t_stack *a, t_stack *b, t_bench *bench);
+int		push_a(t_stack *a, t_stack *b, t_bench *bench);
+int		push_b(t_stack *a, t_stack *b, t_bench *bench);
+int		rotate_a(t_stack *a, t_bench *bench);
+int		rotate_b(t_stack *b, t_bench *bench);
+int		rotate_r(t_stack *a, t_stack *b, t_bench *bench);
+int		rotate_r_a(t_stack *a, t_bench *bench);
+int		rotate_r_b(t_stack *b, t_bench *bench);
+int		rotate_r_r(t_stack *a, t_stack *b, t_bench *bench);
+int		push_stack(t_stack *dest, t_stack *source);
+
+int		parse_stack_input(char **args, int count, t_stack *s);
+int		parse_op(t_stack *a, t_stack *b, char op[4]);
+
+#endif
